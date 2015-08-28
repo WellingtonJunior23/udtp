6246bd97f5e5432f0f2858ee1a4778baaed879af
Márcio Ramos <marciomrs4@hotmail.com>
2015-08-28 17:39:24 -0300

    Create a composer autoload

--- intranet/js/my-timer.js
+++ intranet/js/my-timer.js
@@ -1,19 +1,25 @@
+var $load = jQuery.noConflict();
 
- function startTime() {
-     var today=new Date();
-     var h=today.getHours();
-     var m=today.getMinutes();
-     var s=today.getSeconds();
-     
-     m=checkTime(m);
-     s=checkTime(s);
-     document.getElementById('timer').innerHTML=h+":"+m+":"+s;
-     t=setTimeout('startTime()',500);
- }
+function checkTime(i){
+    if (i<10) {
+        i="0" + i;
+    }
+    return i;
+}
 
- function checkTime(i){
- if (i<10) {
-     i="0" + i;
- }
-     return i;
- }
+
+function mostrarTempo()
+{
+    date = new Date();
+
+    dataAtual = date.getDate() +'/0'+ (date.getMonth()+parseInt(1)) +'/'+ date.getFullYear() + ' | ' +
+    date.getHours() +':'+ checkTime(date.getMinutes())+':'+checkTime(date.getSeconds());
+
+    $load("#timer").html(dataAtual);
+
+    setTimeout('mostrarTempo()',1000);
+
+}
+
+
+mostrarTempo();
--- intranet/src/app/core/Error.php
+++ intranet/src/app/core/Error.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace system\core;
+namespace app\core;
 
 class Error
 {
